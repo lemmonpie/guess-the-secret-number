5bcaeafd994d3ecb81b9530c604c97936a0dc054
lstorm-ux <63728542+lstorm-ux@users.noreply.github.com>
2020-05-15 12:38:40 +0200

    secret-number

---
+++ guess-the-secret-number.py
@@ -0,0 +1,9 @@
+secret = 12345
+
+guess = input("Inter a five digit secret number:")
+
+if guess == "12345":
+    print("Great job! You guessed the right number!")
+
+else:
+    print("The number is incorrect. Give it another shot!")
